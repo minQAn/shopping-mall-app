8129b48fff014596b9775e92dd56357e06e0de8a
minQ <minq.an7@gmail.com>
2022-12-30 12:54:31 -0500

    add router including header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiShoppingBag } from 'react-icons/fi';
+import { BsFillPencilFill } from 'react-icons/bs';
+
+export default function Navbar() {
+  return (
+    <header>
+      <Link to='/'>
+        <FiShoppingBag />
+        <h1>Shopping Mall App</h1>
+      </Link>
+
+      <nav>
+        <Link to='/products'>Products</Link>
+        <Link to='/carts'>Carts</Link>
+        <Link to='/products/new'>
+          <BsFillPencilFill />
+        </Link>
+        <button>Login</button>
+      </nav>
+    </header>
+  );
+}
