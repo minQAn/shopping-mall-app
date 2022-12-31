2ea5dcd9d2c04801339ff884e4b5d70d7da357f1
minQ <minq.an7@gmail.com>
2022-12-31 16:13:33 -0500

    add admin UI and make Button component to make reuseable

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function User({ user: { photoURL, displayName } }) {
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center shrink-0'>
+      {/* shrink-0 is to prevent shrink the image*/}
       <img
         className='w-10 h-10 rounded-full mr-2'
         src={photoURL}
