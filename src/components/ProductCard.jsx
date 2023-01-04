6ee61bb9c31d23e1baac54c07065d4763acf159c
minQ <minq.an7@gmail.com>
2023-01-04 14:14:10 -0500

    add product detail page and UI

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductCard({
+  product,
   product: { id, image, title, category, price },
 }) {
+  const navigate = useNavigate();
   return (
-    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+    <li
+      onClick={() => navigate(`/products/${id}`, { state: { product } })}
+      className='rounded-lg shadow-md overflow-hidden cursor-pointer hover:scale-105 transition-all'
+    >
       <img className='w-full' src={image} alt={title} />
       <div className='mt-2 px-2 text-lg flex justify-between  items-center'>
         <h3 className='truncate'>{title}</h3>
