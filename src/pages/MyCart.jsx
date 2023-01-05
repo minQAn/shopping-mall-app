37223de0cbfdc583c59f76f77ba16ff0ef5418b2
minQ <minq.an7@gmail.com>
2023-01-05 15:49:54 -0500

    add carts UI

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -6,6 +6,7 @@ import CartItem from '../components/CartItem';
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 import { FaEquals } from 'react-icons/fa';
 import PriceCard from '../components/PriceCard';
+import Button from '../components/ui/Button';
 
 const SHIPPING = 3000;
 
@@ -25,24 +26,27 @@ export default function MyCart() {
     ); // 0 is initial value
 
   return (
-    <section>
-      <p>My Cart</p>
+    <section className='p-8 flex flex-col'>
+      <p className='text-2xl text-center font-bold pb-4 border-b border-gray-300'>
+        My Cart
+      </p>
       {!hasProducts && <p>There is no Item</p>}
       {hasProducts && (
         <>
-          <ul>
+          <ul className='border-b border-gray-300 mb-8 p-4 px-8'>
             {products &&
               products.map((product) => (
                 <CartItem key={product.id} uid={uid} product={product} />
               ))}
           </ul>
-          <div>
+          <div className='flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16'>
             <PriceCard text='Total amount of goods' price={totalPrice} />
-            <BsFillPlusCircleFill />
+            <BsFillPlusCircleFill className='shrink-0' />
             <PriceCard text='Delivery Fee' price={SHIPPING} />
-            <FaEquals />
+            <FaEquals className='shrink-0' />
             <PriceCard text='Total Price' price={totalPrice + SHIPPING} />
           </div>
+          <Button text={'Order'} />
         </>
       )}
     </section>
