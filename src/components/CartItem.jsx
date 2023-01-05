2723f3b03e09ff84e0e30a23a68ef624898e1693
minQ <minq.an7@gmail.com>
2023-01-05 12:59:37 -0500

    add Carts, CRUD functions for quantity

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import { addOrUpdateToCart, removeFromCart } from '../api/firebase';
+
+export default function CartItem({
+  uid,
+  product,
+  product: { id, image, title, option, quantity, price },
+}) {
+  const handleMinus = () => {
+    if (quantity < 2) return;
+    addOrUpdateToCart(uid, { ...product, quantity: quantity - 1 });
+  };
+
+  const handlePlus = () =>
+    addOrUpdateToCart(uid, { ...product, quantity: quantity + 1 });
+
+  const handleDelete = () => {
+    removeFromCart(uid, id); // userId, productId
+  };
+
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{option}</p>
+        <div>
+          <AiOutlineMinusSquare onClick={handleMinus} />
+          <span>{quantity}</span>
+          <AiOutlinePlusSquare onClick={handlePlus} />
+          <RiDeleteBin5Fill onClick={handleDelete} />
+        </div>
+      </div>
+    </li>
+  );
+}
--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,5 +1,50 @@
 import React from 'react';
+import { getCart } from '../api/firebase';
+import { useQuery } from '@tanstack/react-query';
+import { useAuthContext } from '../context/AuthContext';
+import CartItem from '../components/CartItem';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
+import { FaEquals } from 'react-icons/fa';
+import PriceCard from '../components/PriceCard';
+
+const SHIPPING = 3000;
 
 export default function MyCart() {
-  return <div>MyCart</div>;
+  const { uid } = useAuthContext();
+  const { isLoading, data: products } = useQuery(['carts'], () => getCart(uid));
+
+  if (isLoading) return <p>Loading...</p>;
+
+  const hasProducts = products && products.length > 0;
+
+  const totalPrice =
+    products &&
+    products.reduce(
+      (prev, current) => prev + parseInt(current.price) * current.quantity,
+      0
+    ); // 0 is initial value
+
+  return (
+    <section>
+      <p>My Cart</p>
+      {!hasProducts && <p>There is no Item</p>}
+      {hasProducts && (
+        <>
+          <ul>
+            {products &&
+              products.map((product) => (
+                <CartItem key={product.id} uid={uid} product={product} />
+              ))}
+          </ul>
+          <div>
+            <PriceCard text='Total amount of goods' price={totalPrice} />
+            <BsFillPlusCircleFill />
+            <PriceCard text='Delivery Fee' price={SHIPPING} />
+            <FaEquals />
+            <PriceCard text='Total Price' price={totalPrice + SHIPPING} />
+          </div>
+        </>
+      )}
+    </section>
+  );
 }
